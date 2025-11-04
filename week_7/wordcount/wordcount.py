825af97b3ed9b06ccaadb26d3dc5daf5f065e6d3
unknown <lolipop22234@gmail.com>
2025-11-04 15:33:13 +0200

    Auto-commit on Tue Nov  4 15:33:12     2025

---
+++ week_7/wordcount/wordcount.py
@@ -0,0 +1,40 @@
+import string
+from operator import itemgetter
+
+
+def reader(filename):
+    dictionary = {}
+    with open(filename) as f:
+        sentence = f.read().lower()
+        sentence = sentence.translate(str.maketrans("", "", string.punctuation))
+        words = sentence.split()
+        for word in words:
+            dictionary[word] = dictionary.get(word, 0) + 1
+        return dictionary
+
+
+def print_words(filename):
+    print("\nPrinting all words")
+    dictionary = reader(filename)
+    for word, count in sorted(dictionary.items()):
+        print(word, count)
+
+
+def print_top(filename):
+    print("\nPrinting top 20 most common words")
+    dictionary = reader(filename)
+    new_dictionary = sorted(dictionary.items(), key=itemgetter(1), reverse=True)
+    for word, count in new_dictionary[:20]:
+        print(word, count)
+
+
+def main():
+    print("=== Word Count Program ===")
+    print_words("small.txt")
+    print()
+    print_top("alice.txt")
+
+
+# вот это ключевая строка
+if __name__ == "__main__":
+    main()
